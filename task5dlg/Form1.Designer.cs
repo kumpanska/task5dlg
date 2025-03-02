0f0bc768973f7d0cc72b7fc3902aeb368e3277b6
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-03-02 14:22:37 +0200

    add delegate and checkboxes

--- task5dlg/Form1.Designer.cs
+++ task5dlg/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
@@ -66,7 +69,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(0, 29);
+            this.button4.Location = new System.Drawing.Point(0, 31);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(237, 23);
             this.button4.TabIndex = 3;
@@ -74,11 +77,47 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.btnSetOfActions_Click);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(0, 60);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(246, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "\"супермегакнопка\" поглинає \"Прозорість\"";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(0, 83);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(236, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "\"супермегакнопка\" поглинає \"Колір тла\"";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(0, 106);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(244, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "\"супермегакнопка\" поглинає \"Hello World\"";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -86,6 +125,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -95,6 +135,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
     }
 }
 
--- task5dlg/Form1.cs
+++ task5dlg/Form1.cs
@@ -10,8 +10,10 @@ using System.Windows.Forms;
 
 namespace task5dlg
 {
+    public delegate void Delegate();
     public partial class Form1 : Form
     {
+        private Delegate dlg;
         public Form1()
         {
             InitializeComponent();
@@ -49,6 +51,26 @@ namespace task5dlg
         private void btnSetOfActions_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Я супермегакнопка,\nі цього мене не позбавиш!");
+            dlg();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            { dlg = button1_Click; }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            { dlg = button2_Click; }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            { dlg = button3_Click; }
+
         }
     }
 }
